540d6dacb077ea4f97065081826872a22bb05756
Ash <30979417+AshAli@users.noreply.github.com>
2018-08-04 17:48:04 +0800

    Finish Lesson 2

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,14 +15,74 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.widget.ListView;
+
+        import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
+
+        //Create an array of words
+        //String wordsArray [] = {"one","two","three","four","five","six","seven","eight","nine","ten"};
+        //ArrayList words = new ArrayList<>(Arrays.asList(wordArray));
+
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+        //Add the word & translation to ArrayList of Custom Word class
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo’e"));
+        words.add(new Word("ten", "na’aacha"));
+
+        // This will create a TextView for every single item in ArrayList (more memory reqd.)
+        /*
+
+        // Find the root view so we can add child views to it
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        // Keep looping until we've reached the end of the list (which means keep looping
+        // as long as the current index position is less than the length of the list)
+        // The index variable keeps track of our current position in the list and
+        // increments by 1 each time the code in the loop is executed.
+        for (int index = 0; index < words.size(); index++) {
+            // Create a new TextView
+            TextView wordView = new TextView(this);
+            // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+        }
+
+        //Logging the values of an the words ArrayList
+        //for (int i = 0; i < words.size(); i++) {
+        //    Log.v("NumbersActivity","Word at index " + i + ": " + words.get(i));
+        //}
+
+        */
+
+        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
+        listView.setAdapter(adapter);
     }
 }
